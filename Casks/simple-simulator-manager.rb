7b712401972234b66f60db0ffd4687ceadcd8f1d
Nicolas Hiller <nicolashiller@icloud.com>
2026-06-17 20:26:09 +0200

    Fix cask audit: drop redundant verified, require Sequoia, sort zap

--- Casks/simple-simulator-manager.rb
+++ Casks/simple-simulator-manager.rb
@@ -2,8 +2,7 @@ cask "simple-simulator-manager" do
   version "1.3.0"
   sha256 "e00eac67e802f86a6087629345b31ac070081f6da2671064d7a456ff79514f4c"
 
-  url "https://github.com/Heckscheibe/SimpleSimulatorManager/releases/download/v#{version}/SimulatorManager.zip",
-      verified: "github.com/Heckscheibe/SimpleSimulatorManager/"
+  url "https://github.com/Heckscheibe/SimpleSimulatorManager/releases/download/v#{version}/SimulatorManager.zip"
   name "Simple Simulator Manager"
   desc "Menu bar tool for inspecting and managing apps on Apple simulators"
   homepage "https://github.com/Heckscheibe/SimpleSimulatorManager"
@@ -13,14 +12,14 @@ cask "simple-simulator-manager" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sequoia
 
   app "Simulator Manager.app"
 
   zap trash: [
     "~/Library/Caches/com.nicolashiller.SimpleSimulatorManager",
     "~/Library/HTTPStorages/com.nicolashiller.SimpleSimulatorManager",
-    "~/Library/Preferences/SimulatorManager.plist",
     "~/Library/Preferences/com.nicolashiller.SimpleSimulatorManager.plist",
+    "~/Library/Preferences/SimulatorManager.plist",
   ]
 end
