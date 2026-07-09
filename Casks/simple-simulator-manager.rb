295f6650006b80e291fe4326c84086261c2ebca6
Nicolas Hiller <nicolas.hiller@porsche.de>
2026-07-09 21:32:34 +0200

    simple-simulator-manager 1.3.1

--- Casks/simple-simulator-manager.rb
+++ Casks/simple-simulator-manager.rb
@@ -1,6 +1,6 @@
 cask "simple-simulator-manager" do
-  version "1.3.0"
-  sha256 "e00eac67e802f86a6087629345b31ac070081f6da2671064d7a456ff79514f4c"
+  version "1.3.1"
+  sha256 "381f487fde6ca3b06baef0778ea0ec595837b0c7d6e436c29cd995744622fac0"
 
   url "https://github.com/Heckscheibe/SimpleSimulatorManager/releases/download/v#{version}/SimulatorManager.zip"
   name "Simple Simulator Manager"
